3fa7eb7d258ffd8d40c30069ef9f956a54ed2c2e
Ruben Martinez <rubenmartinezTRABALLOS@gmail.com>
2025-02-03 10:19:52 +0100

    Agregar configuración del backend con rutas de usuario y nación, modelos, controladores y conexión a MongoDB

---
+++ backend/controllers/nationController.js
@@ -0,0 +1,39 @@
+import Nation from '../models/Nation.js';
+import NationTest from '../models/NationTest.js';
+
+const createNation = async (req, res) => {
+    try {
+        const newNation = new NationTest(req.body);
+        const savedNation = await newNation.save();
+
+        res.send('Nation saved successfully: ', savedNation);
+    } catch (error) {
+        console.log(error);
+        res.send(
+            {msg: "Error creating nation"}
+        );
+    }
+}
+
+const testNation = (req, res) => {
+    console.log(req.body);
+    res.send(
+        {
+            nation_name: req.body.nation_name,
+            historical_context: "req.body.historical_context",
+            geopolitical_context: "req.body.geopolitical_context",
+            politics: "req.body.politics",
+            population: "req.body.population",
+            historical_curiosities: "req.body.historical_curiosities",
+            important_characters: {
+                character1: "peter"
+            },
+            createdAt: "20/11/2002"
+        }
+    );
+}
+
+export {
+    createNation,
+    testNation,
+}
---
+++ backend/routes/nationRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { createNation, testNation } from '../controllers/nationController.js';
+const router = express.Router();
+
+router.post('/', createNation);
+router.get('/', testNation);
+
+export default router;
